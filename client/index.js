701ee528d4bc39e81d7e04bad3de787667c08c8a
Connie Lee <connie.lee0317@gmail.com>
2017-10-04 11:30:49 -0500

    Update map center coordinates

--- client/index.js
+++ client/index.js
@@ -17,9 +17,12 @@ const state = {
 
 mapboxgl.accessToken = "YOUR API TOKEN HERE";
 
+const fullstackCoords = [-74.009, 40.705] // NY
+// const fullstackCoords = [-87.6320523, 41.8881084] // CHI
+
 const map = new mapboxgl.Map({
   container: "map",
-  center: [-74.009, 40.705], // FullStack coordinates
+  center: fullstackCoords,
   zoom: 12, // starting zoom
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
