a06770704aa8e598bb16a5c89394cab4f0170f3e
Nabil Yafai <nabilyafai90@gmail.com>
2018-06-15 19:48:56 -0400

    setting up an attibute to bring in nodes in a time lapse to mock real time

--- public/bundle.js
+++ public/bundle.js
@@ -114,67 +114,50 @@ function getEndLL(d) {
 d3.csv('dataSet.csv').then(function(data) {
 	//console.log(data[0], getLL(data[0]), project(data[0]))
 	var dots = svg.selectAll('circle.dot').data(data);
+	dots
+		.enter()
+		.append('circle')
+		.classed('dot', true)
+		.attr('r', 0.5)
+		.attr('cx', d => {
+			const startTimeArray = d.starttime.split(' ')[1].split(':');
+			const hour = Number(startTimeArray[0] * 3600);
+			const minutes = Number(startTimeArray[1] * 60);
+			const seconds = Number(startTimeArray[2]);
+			const startTimeTotalSeconds = hour + minutes + seconds;
+			console.log('excel', startTimeTotalSeconds);
+			var x = project(d, getStartLL).x;
+			return x;
+		})
+		.attr('cy', d => {
+			var y = project(d, getStartLL).y;
+			return y;
+		})
+		.style('fill', '#00a34c')
+		.style('fill-opacity', 0.6)
+		.style('stroke', '#007c3a')
+		.style('stroke-width', 1)
+		.transition()
+		.duration(1000)
+		.attr('r', 4)
+		.transition()
+		.delay(1000)
+		.style('fill', '#fc2f00')
+		.style('stroke', '#c12300')
+		.style('fill-opacity', 0.6)
+		.attr('cx', d => {
+			var x = project(d, getEndLL).x;
+			return x;
+		})
+		.attr('cy', d => {
+			var y = project(d, getEndLL).y;
+			return y;
+		})
+		.duration(d => {
+			return d.tripduration * 10;
+		});
 
-	function render() {
-		dots
-			.enter()
-			.append('circle')
-			.classed('dot', true)
-			.attr('cx', d => {
-				console.log('d', d);
-				var x = project(d, getStartLL).x;
-				return x;
-			})
-			.attr('cy', d => {
-				var y = project(d, getStartLL).y;
-				return y;
-			})
-			.attr('r', 0.5)
-			.style('fill', '#00a34c')
-			.style('fill-opacity', 0.6)
-			.style('stroke', '#007c3a')
-			.style('stroke-width', 1)
-			.transition()
-			.duration(1000)
-			.attr('r', 4)
-			.transition()
-			.delay(2000)
-			.style('fill', '#fc2f00')
-			.style('stroke', '#c12300')
-			.style('fill-opacity', 0.6)
-			.duration(1000)
-			.attr('cx', d => {
-				console.log('d', d);
-				var x = project(d, getEndLL).x;
-				return x;
-			})
-			.attr('cy', d => {
-				var y = project(d, getEndLL).y;
-				return y;
-			});
-
-		// dots
-		// dots
-		// 	.transition()
-		// 	.attr({
-		// 		cx: function(d) {
-		// 			var x = project(d, getEndLL).x;
-		// 			return x;
-		// 		},
-		// 		cy: function(d) {
-		// 			var y = project(d, getEndLL).y;
-		// 			return y;
-		// 		}
-		// 	})
-		// .style({
-		// 	fill: 'red',
-		// 	'fill-opacity': 0.6,
-		// 	stroke: 'red',
-		// 	'stroke-width': 1
-		// })
-		// .delay(1000)
-		// .duration(4000);
-	}
+	function render() {}
 
 	// re-render our visualization whenever the view changes
 	map.on('viewreset', function() {
@@ -189,10 +172,10 @@ d3.csv('dataSet.csv').then(function(data) {
 });
 
 // Create the marker
-const marker = buildMarker(null, centralParkCoords);
+// const marker = buildMarker(null, centralParkCoords);
 
 //ADD TO DOM
-marker.addTo(map);
+// marker.addTo(map);
 
 // Animate the map
 // map.flyTo({ center: attraction.place.location, zoom: 15 });
