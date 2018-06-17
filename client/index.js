1fd0a62c8b76d1f96ea2b27aa72693200ceb1009
Nabil Yafai <nabilyafai90@gmail.com>
2018-06-17 15:41:22 -0400

    adding legend for data color scheme

--- client/index.js
+++ client/index.js
@@ -287,19 +287,3 @@ function getMostPopularBike(frequencyObj) {
 	});
 	return freqArr[0];
 }
-
-var pipsSteps = document.getElementById('pips-steps');
-
-noUiSlider.create(pipsSteps, {
-	range: range_all_sliders,
-	start: 0,
-	pips: {
-		mode: 'steps',
-		density: 3,
-		filter: filter500,
-		format: wNumb({
-			decimals: 2,
-			prefix: '$'
-		})
-	}
-});
--- public/bundle.js
+++ public/bundle.js
@@ -9825,22 +9825,6 @@ function getMostPopularBike(frequencyObj) {
 	return freqArr[0];
 }
 
-var pipsSteps = document.getElementById('pips-steps');
-
-noUiSlider.create(pipsSteps, {
-	range: range_all_sliders,
-	start: 0,
-	pips: {
-		mode: 'steps',
-		density: 3,
-		filter: filter500,
-		format: wNumb({
-			decimals: 2,
-			prefix: '$'
-		})
-	}
-});
-
 
 /***/ }),
 /* 174 */
